99a94e04161125dacb66906318567020b13b2eea
Dominion1212 <dominionogbaji15@gmail.com>
2023-06-02 12:17:21 +0100

    added gomarket

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Go-market app
         </a>
       </header>
     </div>
